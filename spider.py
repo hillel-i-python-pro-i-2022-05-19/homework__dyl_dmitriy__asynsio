2ae0ce31b3a14e05d781c96539a19b2288670ee3
DylDmitriy <dyl.dmitriy@gmail.com>
2022-06-12 01:03:57 +0300

    as4

--- spider.py
+++ spider.py
@@ -1,7 +1,13 @@
 import scrapy
 
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy import signals
+from scrapy.http import Request
+from scrapy.utils.trackref import object_ref
+from scrapy.utils.url import url_is_from_spider
+
 from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
 
 #https://www.yakaboo.ua/ua/knigi.html
 #https://www.yakaboo.ua/ua/knigi/hudozhestvennaja-literatura.html
@@ -21,9 +27,11 @@ class BookSpider(CrawlSpider):
         yield{
             "First":response.css('a.breadcrumbs__link.smartLink span::text')[1].get().strip(),
             "Second":response.css('a.breadcrumbs__link.smartLink span::text')[2].get().strip(),
-            "Third":response.css('a.breadcrumbs__link.smartLink span::text')[3].get().strip()
+            #"Third":response.css('a.breadcrumbs__link.smartLink span::text')[3].get().strip()
             "name": response.css('h1.product-detail-page__title::text').get().strip(),
 
             "buy": response.css('p.product-detail-page__purchased-text::text').get().split()[1],
 
         }
+
+
